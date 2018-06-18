cff3823167a3a06c2dc808684cc6a9517875b665
gonzodamus <github email address>
2018-06-18 16:18:13 -0400

    rendering only necessary info up front

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,8 @@ class RestaurantsController < ApplicationController
 
   def index
     restaurants = Restaurant.all.sort{|a,b| a.name <=> b.name}
-    render json: restaurants.first(20)
+    restaurant_names = restaurants.map{|restaurant| {id: restaurant.id, name: restaurant.name, park:restaurant.park}}
+    render json: restaurant_names
   end
 
   def show
