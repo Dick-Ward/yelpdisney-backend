c975f3a3ac30c29bde170bf7aae9fc12551c8f22
gonzodamus <github email address>
2018-07-19 11:46:03 -0400

    average rating

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,8 +1,15 @@
 class RestaurantSerializer < ActiveModel::Serializer
+  attributes :average_rating :details
   has_many :reviews
 
-  def attributes(*args)
+  def details(*args)
     object.attributes.symbolize_keys
+  end
+
+  def average_rating
+    ratings = object.reviews.map{|review| review.rating}
+    sum = ratings.reduce(:+)
+    (sum / ratings.length.to_f).round(1)
   end
 
 end
