1524b4bf4f4188ecc9b3a92a6b0552cffbd5520a
gonzodamus <github email address>
2018-07-19 11:56:58 -0400

    changed serializer to account for no reviews

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,15 +1,15 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :average_rating, :details
   has_many :reviews
-
-  def details(*args)
-    object.attributes.symbolize_keys
-  end
+  attributes :average_rating, :name, :permalink, :category_code, :portion_size, :cost_code, :cuisine, :entree_range, :parking, :bar, :wine_list, :dress, :awards, :breakfast_hours, :lunch_hours, :dinner_hours, :house_specialties, :requires_credit_card, :requires_pre_payment, :opened_on, :disney_permalink, :code, :short_name, :accepts_reservations, :kosher_available, :location_details, :operator_id, :operator_url, :operator_type, :walking_time_proxy_id, :sort_name, :mobile_ordering, :park, :resort_name
 
   def average_rating
-    ratings = object.reviews.map{|review| review.rating}
-    sum = ratings.reduce(:+)
-    (sum / ratings.length.to_f).round(1)
+    if object.reviews.length > 0
+      ratings = object.reviews.map{|review| review.rating}
+      sum = ratings.reduce(:+)
+      (sum / ratings.length.to_f).round(1)
+    else
+      0
+    end
   end
 
 end
