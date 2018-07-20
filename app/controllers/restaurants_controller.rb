d5b415748286cc9d8cd31aca00d1de925921f674
gonzodamus <github email address>
2018-07-20 14:57:59 -0400

    added id to serializer

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,7 @@ class RestaurantsController < ApplicationController
 
   def index
     restaurants = Restaurant.all.sort{|a,b| a.name.gsub(/\W/, '') <=> b.name.gsub(/\W/, '')}
-    restaurant_names = restaurants.map{|restaurant| {id: restaurant.id, name: restaurant.name, park:restaurant.park}}
+    restaurant_names = restaurants.map{|restaurant| {id: restaurant.id, name: restaurant.name, park:restaurant.park, resort_name: restaurant.resort_name}}
     render json: restaurant_names
   end
 
--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,6 +1,6 @@
 class RestaurantSerializer < ActiveModel::Serializer
   has_many :reviews
-  attributes :average_rating, :name, :permalink, :category_code, :portion_size, :cost_code, :cuisine, :entree_range, :parking, :bar, :wine_list, :dress, :awards, :breakfast_hours, :lunch_hours, :dinner_hours, :house_specialties, :requires_credit_card, :requires_pre_payment, :opened_on, :disney_permalink, :code, :short_name, :accepts_reservations, :kosher_available, :location_details, :operator_id, :operator_url, :operator_type, :walking_time_proxy_id, :sort_name, :mobile_ordering, :park, :resort_name
+  attributes :average_rating, :id, :name, :permalink, :category_code, :portion_size, :cost_code, :cuisine, :entree_range, :parking, :bar, :wine_list, :dress, :awards, :breakfast_hours, :lunch_hours, :dinner_hours, :house_specialties, :requires_credit_card, :requires_pre_payment, :opened_on, :disney_permalink, :code, :short_name, :accepts_reservations, :kosher_available, :location_details, :operator_id, :operator_url, :operator_type, :walking_time_proxy_id, :sort_name, :mobile_ordering, :park, :resort_name
 
   def average_rating
     if object.reviews.length > 0
