0d36bfdcfee63e8dea56e2bbba2a408cbbb50dd6
Gonzodamus <Dick@TheDickWard.com>
2018-09-15 14:17:21 -0400

    adjusted show to accept permalink

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,12 +2,12 @@ class RestaurantsController < ApplicationController
 
   def index
     restaurants = Restaurant.all.sort{|a,b| a.name.gsub(/\W/, '') <=> b.name.gsub(/\W/, '')}
-    restaurant_names = restaurants.map{|restaurant| {id: restaurant.id, name: restaurant.name, park:restaurant.park, resort_name: restaurant.resort_name}}
+    restaurant_names = restaurants.map{|restaurant| {id: restaurant.id, name: restaurant.name, park:restaurant.park, resort_name: restaurant.resort_name, permalink: restaurant.permalink}}
     render json: restaurant_names
   end
 
   def show
-    restaurant = Restaurant.find(params[:id])
+    restaurant = Restaurant.find_by(permalink: params[:id])
     render json: restaurant
   end
 
