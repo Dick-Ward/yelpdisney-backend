f8f2454ac8393970faa4a62ff252d7dec2cbb4fc
gonzodamus <github email address>
2018-11-02 17:08:30 -0400

    queries improved, but I know the ratings average could be better

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,11 +6,31 @@ class Restaurant < ApplicationRecord
 
   def average_rating
     if self.reviews.length > 0
-      average = Review.where(restaurant_id: self.id).pluck("avg(quality) + avg(cleanliness) + avg(service) + avg(value)")[0].to_f/4
-      number_with_precision(average, precision: 2).to_f
+      average = (average_quality + average_cleanliness + average_service + average_value) / 4
+      return number_with_precision(average, precision: 2).to_f
     else
       0
     end
+  end
+
+  def average_quality
+    average = Review.where(restaurant_id: self.id).average(:quality)
+    return number_with_precision(average, precision: 2).to_f
+  end
+
+  def average_cleanliness
+    average = Review.where(restaurant_id: self.id).average(:cleanliness)
+    return number_with_precision(average, precision: 2).to_f
+  end
+
+  def average_service
+    average = Review.where(restaurant_id: self.id).average(:service)
+    return number_with_precision(average, precision: 2).to_f
+  end
+
+  def average_value
+    average = Review.where(restaurant_id: self.id).average(:value)
+    return number_with_precision(average, precision: 2).to_f
   end
 
 
--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,4 +1,4 @@
 class RestaurantSerializer < ActiveModel::Serializer
-
-  has_many :reviews
+  attributes :average_rating, :average_quality, :average_cleanliness, :average_service, :average_value, :id, :name, :permalink, :category_code, :portion_size, :cost_code, :cuisine, :entree_range, :parking, :bar, :wine_list, :dress, :awards, :breakfast_hours, :lunch_hours, :dinner_hours, :requires_credit_card, :requires_pre_payment, :opened_on, :disney_permalink, :accepts_reservations, :kosher_available, :location_details, :operator_url, :operator_type, :walking_time_proxy_id,  :mobile_ordering, :park, :resort_name
+  has_many :reviews, serializer: ReviewSerializer
 end
